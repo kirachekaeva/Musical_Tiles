3dc2d93596f71e7039b3c83d43aed09ea6817b83
kirachekaeva <m2408945@edu.misis.ru>
2025-06-05 10:50:14 +0900

    Program.cs

--- MT/MT/Program.cs
+++ MT/MT/Program.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using Model.Data;
 
 namespace MT
 {
@@ -10,7 +13,30 @@ namespace MT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            InitializeScoresFile();
+
             Application.Run(new MainMenuForm());
+        }
+
+        private static void InitializeScoresFile()
+        {
+            try
+            {
+                string scoresPath = "scores.json";
+                if (!File.Exists(scoresPath))
+                {
+                    var emptyScores = new List<int>();
+                    new JsonScoreSerializer().Serialize(emptyScores);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не удалось инициализировать файл рекордов: {ex.Message}",
+                              "Ошибка инициализации",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Error);
+            }
         }
     }
 }
